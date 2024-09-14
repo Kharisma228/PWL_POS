c5e3d3177cee852f7655d8a1802e52ef14a018a6
Kharisma Etana Febrianto <kharisma_etana@polinema.ac.id>
2024-09-14 23:13:06 +0700

    JS 3 - Praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,5 +12,10 @@ Route::get('/', function () {
 /* Praktikum 4 */
 
 Route::get('/level', [LevelController::class, 'index']);
+
+
+/* Praktikum 5 */
+
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 
