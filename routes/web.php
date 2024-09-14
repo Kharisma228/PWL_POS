8998c574fd791d19e7dc139da8ea79120e97e078
Kharisma Etana Febrianto <kharisma_etana@polinema.ac.id>
2024-09-14 22:42:04 +0700

    JS 3 - Praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/* Praktikum 4 */
+
+Route::get('/level', [LevelController::class, 'index']);
+
+
