c9aaf108161a8b539a4efae4523aea5c77e40cc7
Kharisma Etana Febrianto <kharisma_etana@polinema.ac.id>
2024-09-18 16:24:52 +0700

    JS 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,12 +11,21 @@ class UserController extends Controller
 {
     public function index()
     {
+        // JS 4 PRAKTIKUM 2.2
 
-        $user = UserModel::findOr(20, ['username', 'name'], function() {
+        $user = UserModel::where('username', 'managers9')->firstOrFail();
+        return view('user' , ['data' => $user]);
+
+        /*$user = UserModel::findOrFail(1);
+        return view('user' , ['data' => $user]);*/
+
+
+        // JS 4 PRAKTIKUM 2.1
+        /*$user = UserModel::findOr(20, ['username', 'name'], function() {
             abort(404);
         });
 
-        return view('user', ['data' => $user]);
+        return view('user', ['data' => $user]);*/
 
         /*$user = UserModel::findOr(1, ['username', 'name'], function() {
             abort(404);
@@ -33,6 +42,8 @@ class UserController extends Controller
         /*$user = UserModel::find(1);
         return view('user', ['data' => $user]);*/
         
+
+        // JS 4 PRAKTIKUM 1
         /* $data = [
             'level_id' => 2,
             'username' => 'manager_dua',
@@ -50,6 +61,9 @@ class UserController extends Controller
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);*/
+
+
+        // JS 2 PRAKTIKUM
 
         /*$data = [
         'name' => 'Pelanggan Pertama'
